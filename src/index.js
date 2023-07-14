56789c5dbf75c030cf303d429ac99640048d9139
Talha Momin <77355191+momintlh@users.noreply.github.com>
2023-07-14 15:39:59 +0500

    multiplayer test

--- src/index.js
+++ src/index.js
@@ -1,49 +1,76 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-import {isStreamScreen, onPlayerJoin, insertCoin, isHost, myPlayer } from "playroomkit";
+import Phaser from "phaser";
+import { onPlayerJoin, insertCoin, isHost, myPlayer, isStreamScreen } from "playroomkit";
+ 
+class Main extends Phaser.Scene {
+  controls = {};
+  players = []; 
+ 
+  create() {
+    // 1. Handle players joining and quiting.
+    onPlayerJoin(playerState => this.addPlayer(playerState));
+ 
+   
+  }
+ 
+  addPlayer(playerState) {
+    const sprite = this.add.rectangle(
+      Phaser.Math.Between(100, 500), 200, 50, 50, playerState.getProfile().color.hex);
+    this.physics.add.existing(sprite, false);
+    sprite.body.setCollideWorldBounds(true);
+    this.players.push({
+      sprite,
+      state: playerState
+    });
+    playerState.onQuit(() => {
+      sprite.destroy();
+      this.players = this.players.filter(p => p.state !== playerState);
+    });
+  }
+ 
+  update() {
+    // 3. Pass your game state to Playroom.
+    if (isHost()){
+      for (const player of this.players) {
+
+        // TODO: Movement goes here
+
+        player.state.setState("pos", {
+          x: player.sprite.body.x,
+          y: player.sprite.body.y,
+        });
+      }
+    }
+    else{
+      for (const player of this.players) {
+        const pos = player.state.getState("pos");
+        if (pos){
+          player.sprite.body.x = pos.x;
+          player.sprite.body.y = pos.y;
+        }
+      }
+    }
+  }
+}
+ 
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: 'root',
+  backgroundColor: '#3498db',
+  physics: {
+    default: 'arcade',
+  },
+  scene: Main
+};
 
 if (isStreamScreen()) {
     // Show the Stream screen
   } else {
-    // Show the Controller screen
+    // TODO: Show the Controller screen on mobile
   }
-
-class MyGame extends Phaser.Scene {
-    constructor() {
-        super();
-    }
-
-    preload() {
-        this.load.image('logo', logoImg);
-    }
-
-    create() {
-
-        const logo = this.add.image(400, 150, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-
-}
-
-
-const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
-};
-
-
-
-insertCoin({streamMode: true}).then(() => {
-    const game = new Phaser.Game(config);
+ 
+// 4. Insert Coin! Start the game.
+insertCoin({streamMode: false}).then(() => {
+  const game = new Phaser.Game(config);
 });
